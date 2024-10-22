1541d3135e920311c0211ea1d45f3c6ca54460e2
João Correia <jpscorreia.dev@gmail.com>
2024-10-22 06:55:29 +0100

    icon, fix to image loading

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
-import MonsterIcon from './MonsterIcon';
 import { useState, useEffect } from 'react';
 import '../Result.css';
 
 const Result = ({ weapon, monster }) => {
   const [weaponImageSrc, setWeaponImageSrc] = useState(null);
+  const [monsterImageSrc, setMonsterImageSrc] = useState(null);
+  const [versusImageSrc, setVersusImageSrc] = useState(null);
 
   useEffect(() => {
-    // Dynamically import the weapon image from the assets/icons folder
+    // Carregar a imagem da arma dinamicamente
     const loadWeaponIcon = async () => {
       try {
         if (weapon) {
@@ -15,48 +16,82 @@ const Result = ({ weapon, monster }) => {
           setWeaponImageSrc(image.default);
         }
       } catch (err) {
-        console.error('Error loading weapon image:', err);
+        console.error('Erro ao carregar a imagem da arma:', err);
+      }
+    };
+
+    // Carregar a imagem do monstro dinamicamente
+    const loadMonsterIcon = async () => {
+      try {
+        if (monster) {
+          const image = await import(`../assets/icons/${monster}.webp`);
+          setMonsterImageSrc(image.default);
+        }
+      } catch (err) {
+        console.error('Erro ao carregar a imagem do monstro:', err);
+      }
+    };
+
+    // Carregar a imagem "versus" dinamicamente
+    const loadVersusIcon = async () => {
+      try {
+        const image = await import(`../assets/images/versus.png`);
+        setVersusImageSrc(image.default);
+      } catch (err) {
+        console.error('Erro ao carregar a imagem "versus":', err);
       }
     };
 
     loadWeaponIcon();
-  }, [weapon]);
+    loadMonsterIcon();
+    loadVersusIcon();
+  }, [weapon, monster]);
 
   if (!weapon || !monster) return null;
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
       <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-      <h2>Quest</h2>
-      <div className="result-container">
+        <h2>Quest</h2>
+        <div className="result-container">
 
-        <div className="weapon-container" style={{  width: '240px' }}>
-          <h4>{weapon}</h4>
-          <div style={{ width: '120px', height: '120px' }}>
-          {weaponImageSrc && (
-            <img
-              src={weaponImageSrc}
-              alt={weapon}
-              style={{ width: '110px', height: '110px' }}
-            />
-          )}
+          <div className="weapon-container" style={{  width: '240px' }}>
+            <h4>{weapon}</h4>
+            <div style={{ width: '120px', height: '120px' }}>
+              {weaponImageSrc && (
+                <img
+                  src={weaponImageSrc}
+                  alt={weapon}
+                  style={{ width: '110px', height: '110px' }}
+                />
+              )}
+            </div>
           </div>
-        </div>
 
-        <div style={{ height: '60px' }}>
-          <img 
-            src="/src/assets/images/versus.png" 
-            alt="versus"
-            style={{ width: '100px', height: '100px', margin: '5px' }}
-          />
-        </div>
+          <div style={{ height: '60px' }}>
+            {versusImageSrc && (
+              <img 
+                src={versusImageSrc} 
+                alt="versus"
+                style={{ width: '100px', height: '100px', margin: '5px' }}
+              />
+            )}
+          </div>
 
-        <div className="monster-container" style={{  width: '240px' }}>
-          <h4>{monster}</h4>
-          {monster && <MonsterIcon imageName={monster} />}
-        </div>
+          <div className="monster-container" style={{  width: '240px' }}>
+            <h4>{monster}</h4>
+            <div style={{ width: '120px', height: '120px' }}>
+              {monsterImageSrc && (
+                <img 
+                  src={monsterImageSrc} 
+                  alt={monster} 
+                  style={{ width: '120px', height: '120px' }} 
+                />
+              )}
+            </div>
+          </div>
 
-      </div>
+        </div>
       </div>
     </div>
   );
