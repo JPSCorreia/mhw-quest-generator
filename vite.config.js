800f5fc2378a1c4520a9b772a8dcf205d588fd1f
João Correia <jpscorreia.dev@gmail.com>
2024-10-22 06:44:33 +0100

    prepare for gh pages 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mhw-quest-generator/', // Define o nome do repositório aqui
+  build: {
+    outDir: 'dist', // Pasta de saída do build para o Vite
+  },
   assetsInclude: ['**/*.webp'],
 })
